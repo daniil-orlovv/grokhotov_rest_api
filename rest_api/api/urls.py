2cd1fcae3a1f5eb9c821258134b8a7c78d233467
Даниил Орлов <danilorlov9898@yandex.ru>
2024-03-18 18:02:14 +0300

    Added endpoints for views

--- rest_api/api/urls.py
+++ rest_api/api/urls.py
@@ -1,15 +1,16 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from views import ListRetrieveBook
+from api.views import ListRetrieveBook, ListCategory
 
 
 router = SimpleRouter()
 
 
-router.register('books', ListRetrieveBook, basename='onlyread_books')
+router.register('books', ListRetrieveBook, basename='books')
 
 
 urlpatterns = [
     path('v1/', include(router.urls)),
+    path('v1/category/', ListCategory.as_view()),
 ]
