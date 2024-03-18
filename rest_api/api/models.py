8d89e08a73d8e82594bf4d6547734bd9f12ac910
Даниил Орлов <danilorlov9898@yandex.ru>
2024-03-18 18:01:20 +0300

    Added models for Book, Author, Category, Feedback

--- rest_api/api/models.py
+++ rest_api/api/models.py
@@ -1,15 +1,45 @@
 from django.db import models
 
 
+class Category(models.Model):
+    title = models.TextField()
+
+
+class Author(models.Model):
+    name = models.TextField()
+
+
 class Book(models.Model):
     title = models.TextField()
-    isbn = models.TextField()
-    pageCount = models.IntegerField()
-    publishedDate = models.TextField()
-    thumbnailUrl = models.TextField()
+    isbn = models.TextField(null=True, blank=True)
+    pagecount = models.IntegerField(null=True, blank=True)
+    publisheddate = models.TextField(null=True, blank=True)
+    thumbnailurl = models.TextField(null=True, blank=True)
+    shortdescription = models.TextField(null=True, blank=True)
+    longdescription = models.TextField(null=True, blank=True)
     status = models.TextField()
-    authors = models.JSONField()
-    categories = models.JSONField()
+    authors = models.ManyToManyField(
+        Author,
+        blank=True
+    )
+    categories = models.ManyToManyField(
+        Category,
+        blank=True
+    )
+
+
+class Subcategory(models.Model):
+    title = models.TextField()
+    book = models.ForeignKey(
+        Book,
+        on_delete=models.CASCADE,
+        related_name='book_subcategories'
+    )
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='category_subcategories'
+    )
 
 
 class FeedBack(models.Model):
