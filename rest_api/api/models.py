414db9a3de84aaab40cbd43fb2c6f699f365e972
Даниил Орлов <danilorlov9898@yandex.ru>
2024-03-20 22:24:15 +0300

    1. Added models for db. 2. Added relationships for Book with Author, Category, Subcategory and for Category with Subcategory

--- rest_api/api/models.py
+++ rest_api/api/models.py
@@ -4,9 +4,27 @@ from django.db import models
 class Category(models.Model):
     title = models.TextField()
 
+    def __str__(self):
+        return self.title
+
+
+class SubCategory(models.Model):
+    title = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name='category_subcategory'
+    )
+
+    def __str__(self):
+        return self.title
+
 
 class Author(models.Model):
     name = models.TextField()
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
@@ -17,33 +35,32 @@ class Book(models.Model):
     thumbnailurl = models.TextField(null=True, blank=True)
     shortdescription = models.TextField(null=True, blank=True)
     longdescription = models.TextField(null=True, blank=True)
-    status = models.TextField()
+    status = models.TextField(blank=True)
     authors = models.ManyToManyField(
         Author,
-        blank=True
+        blank=True,
+        related_name='authors_book'
     )
     categories = models.ManyToManyField(
         Category,
-        blank=True
+        blank=True,
+        related_name='categories_book'
+    )
+    subcategories = models.ManyToManyField(
+        SubCategory,
+        blank=True,
+        related_name='subcategories_book'
     )
 
-
-class Subcategory(models.Model):
-    title = models.TextField()
-    book = models.ForeignKey(
-        Book,
-        on_delete=models.CASCADE,
-        related_name='book_subcategories'
-    )
-    category = models.ForeignKey(
-        Category,
-        on_delete=models.CASCADE,
-        related_name='category_subcategories'
-    )
+    def __str__(self):
+        return self.title
 
 
 class FeedBack(models.Model):
-    email = models.EmailField()
-    name = models.TextField()
-    comment = models.TextField()
-    phone = models.TextField()
+    email = models.EmailField(blank=True)
+    name = models.TextField(blank=True)
+    comment = models.TextField(blank=True)
+    phone = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.comment
