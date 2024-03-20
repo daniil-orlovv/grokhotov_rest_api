e742a70c57e7cda9f3c591950b12b82904faae90
Даниил Орлов <danilorlov9898@yandex.ru>
2024-03-20 22:22:05 +0300

    1. Added classes for admin-zone. 2. Added inlines: author, category, subcategory  for Book

--- rest_api/api/admin.py
+++ rest_api/api/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (Book, Author, Category, SubCategory)
+
+
+@admin.register(SubCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+
+
+class SubCategoryInlineAdmin(admin.TabularInline):
+    model = Book.subcategories.through
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'title',
+    )
+
+
+class CategoryInlineAdmin(admin.TabularInline):
+    model = Book.categories.through
+    extra = 1
+
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+    )
+
+
+class AuthorInlineAdmin(admin.TabularInline):
+    model = Book.authors.through
+    extra = 1
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    inlines = [
+        AuthorInlineAdmin,
+        CategoryInlineAdmin,
+        SubCategoryInlineAdmin,]
+    list_display = (
+        'title',
+        'isbn',
+        'pagecount',
+        'publisheddate',
+        'thumbnailurl',
+        'shortdescription',
+        'longdescription',
+        'status',
+    )
+    list_filter = ('categories',
+                   'title',
+                   'status',
+                   'publisheddate',)
